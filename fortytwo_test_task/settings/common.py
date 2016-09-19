630f8112fc4072ff8120c54dc931d8909eec8b66
Andreev Boghdan <boghdanandreev@gmail.com>
2016-09-19 16:24:22 +0300

    cars2

--- fortytwo_test_task/settings/common.py
+++ fortytwo_test_task/settings/common.py
@@ -80,13 +80,12 @@ WSGI_APPLICATION = 'fortytwo_test_task.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',  # Add database
-        'OPTIONS': {'init_command': 'SET storage_engine=MyISAM', },
-        'NAME': '42_test',
-        'USER': 'root',
-        'PASSWORD': '',
-        'HOST': '',
-        'PORT': '',
+        'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': 'database.sqlite3',                      # Or path to database file if using sqlite3.
+        'USER': '',                      # Not used with sqlite3.
+        'PASSWORD': '',                  # Not used with sqlite3.
+        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
+        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
     }
 }
 
